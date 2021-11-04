6daedc2de3bf8c29fe6d4eb416aac432e73ab19d
fdcrespi <fdcrespi@gmail.com>
2021-11-04 13:13:03 -0300

    Se agrego navigator

--- App.js
+++ App.js
@@ -1,76 +1,44 @@
 import React, { useCallback } from "react";
-import {Text , View, StyleSheet, Image, Linking, Button} from "react-native";
-import {Video} from 'expo-av';
-import imagePause from './assets/ondas-sonoras-pause.png';
-import imagePlay from './assets/ondas-sonoras.gif';
+import {StyleSheet, Linking, Button} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 
-const instagramURL = "https://www.instagram.com/mcymtresarroyos/?utm_medium=copy_link";
-const youtubeURL = "https://www.youtube.com/c/MCyMTresArroyos";
+import HomePage from './components/home'
 
-const OpenURLButton = ({ url, children }) => {
-  const handlePress = useCallback(async () => {
-    const supported = await Linking.canOpenURL(url);
-    if (supported) {
-      await Linking.openURL(url);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
-  }, [url]);
 
-  return <Button title={children} onPress={handlePress} />;
-};
+function HomeScreen({ navigation }) {
+  return (
+    <HomePage />
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+        <DrawerItem
+          label="Cerrar"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}> Tabernaculo de los milagros </Text>
-      <Image style={styles.image} source={status.isPlaying ? imagePlay : imagePause}/>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'https://node-09.zeno.fm/fnge482h268uv?rj-ttl=5&rj-tok=AAABfOY_WzkAQa2hCQKBbGYZyw',
-        }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      <View style={styles.buttonContainer}>
-        <OpenURLButton styles={styles.button} url={instagramURL}>Instagram</OpenURLButton>
-        <OpenURLButton styles={styles.button} url={youtubeURL}>YouTube</OpenURLButton>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: 'rgb(4, 2, 5)'
-  },
-  title: {
-    textAlign: "center",
-    color: 'white',
-    fontSize: 30,
-  },
-  video: {
-    height: 50,
-  },
-  image: {
-    width: 300,
-    height: 150,
-    marginBottom: 5,
-  },
-  buttonContainer: {
-    marginTop: 10,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  }
-});
 
 export default App;
---
+++ components/home.js
@@ -0,0 +1,76 @@
+import React, { useCallback } from "react";
+import {Text , View, StyleSheet, Image, Linking, Button} from "react-native";
+import {Video} from 'expo-av';
+import imagePause from '../assets/ondas-sonoras-pause.png';
+import imagePlay from '../assets/ondas-sonoras.gif';
+
+const instagramURL = "https://www.instagram.com/mcymtresarroyos/?utm_medium=copy_link";
+const youtubeURL = "https://www.youtube.com/c/MCyMTresArroyos";
+
+const OpenURLButton = ({ url, children }) => {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url);
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`la URL no se puede abrir: ${url}`);
+    }
+  }, [url]);
+
+  return <Button title={children} onPress={handlePress} />;
+};
+
+const HomeScreen = () => {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Tabernaculo de los milagros </Text>
+      <Image style={styles.image} source={status.isPlaying ? imagePlay : imagePause}/>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://node-09.zeno.fm/fnge482h268uv?rj-ttl=5&rj-tok=AAABfOY_WzkAQa2hCQKBbGYZyw',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttonContainer}>
+        <OpenURLButton styles={styles.button} url={instagramURL}>Instagram</OpenURLButton>
+        <OpenURLButton styles={styles.button} url={youtubeURL}>YouTube</OpenURLButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgb(4, 2, 5)'
+  },
+  title: {
+    textAlign: "center",
+    color: 'white',
+    fontSize: 30,
+  },
+  video: {
+    height: 50,
+  },
+  image: {
+    width: 300,
+    height: 150,
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+});
+
+export default HomeScreen;

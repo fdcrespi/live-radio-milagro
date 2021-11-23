5bedc8fbff41b0e011809e2261dd703d843dde29
Guido Pisarra <pisarraguido@gmail.com>
2021-11-23 20:47:22 -0300

    form de pedido de oracion

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet } from 'react-native';
+import React, { useCallback } from "react";
+import {StyleSheet, Linking, Button, View, Text} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
-  DrawerItem,
+  DrawerItem
 } from '@react-navigation/drawer';
-import * as Font from 'expo-font';
 
-
-
-import HomePage from './components/home'
-
+import HomePage from './components/home';
+import PrayerRequest from './components/prayerRequest';
 
 function HomeScreen({ navigation }) {
   return (
     <HomePage />
   );
 }
+function prayerReq() {
+  return (
+    
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <PrayerRequest/>
+      </View>
+    
+      
+  );
+}
+
+
 
 function CustomDrawerContent(props) {
   return (
@@ -27,58 +36,44 @@ function CustomDrawerContent(props) {
         
         <DrawerItem
           label="Cerrar"
-          onPress={() => props.navigation.closeDrawer()}
+          onPress={() => this.prayerReq()}
         />
-        
     </DrawerContentScrollView>
   );
 }
 
 const Drawer = createDrawerNavigator();
 
-const App = () => {
-
-  const[fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    if(!fontsLoaded) {
-      loadFonts();
-    }
-  });
-
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      'Glacial': require('./assets/fonts/GlacialIndifference-Bold.otf'),
-      'Waverly': require('./assets/fonts/WaverlyCF-Bold.otf'),
-    });
-    setFontsLoaded(true);
-  };
-
+function MyDrawer() {
   return (
-    <NavigationContainer theme={MyTheme} style={style.nav}>
-      <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="Radio en Vivo" component={HomeScreen} />
-      </Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Notifications" component={Notifications} />
+    </Drawer.Navigator>
+  );
+}
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      
+       <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Radio en Vivo" component={HomeScreen} >
+        </Drawer.Screen>
+        <Drawer.Screen name="Pedidos de Oración" component={prayerReq} >
+      
+      </Drawer.Screen>
+      
+       
+        
+        
+      </Drawer.Navigator> 
+      
     </NavigationContainer>
   );
 };
-
-const MyTheme = {
-  dark: false,
-  colors: {
-    primary: 'black',
-    background: 'rgb(242, 242, 242)',
-    card: '#96613f',
-    text: 'rgb(28, 28, 30)',
-    border: 'rgb(199, 199, 204)',
-    notification: 'rgb(255, 69, 58)',
-  }
-};
-
-const style = StyleSheet.create({
-  nav: {
-    fontFamily: 'Glacial',
-  }
-});
 
 export default App;
--- components/home.js
+++ components/home.js
@@ -1,18 +1,14 @@
 import { useCallback } from "react";
 import * as React from "react";
-import { Text, View, StyleSheet, Image, Linking, Button } from "react-native";
-import { Video } from "expo-av";
-import imagePause from "../assets/ondas-sonoras-pause.png";
-import imagePlay from "../assets/ondas-sonoras.gif";
-import microphone from "../assets/microphone.png";
-import icono from "../assets/templo.png";
+import {Text , View, StyleSheet, Image, Linking, Button} from "react-native";
+import {Video} from 'expo-av';
+import imagePause from '../assets/ondas-sonoras-pause.png';
+import imagePlay from '../assets/ondas-sonoras.gif';
 
-import { FontAwesome, AntDesign, Foundation, MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome } from '@expo/vector-icons';
 
-const instagramURL =  "https://www.instagram.com/mcymtresarroyos/?utm_medium=copy_link";
+const instagramURL = "https://www.instagram.com/mcymtresarroyos/?utm_medium=copy_link";
 const youtubeURL = "https://www.youtube.com/c/MCyMTresArroyos";
-const whatsappURL = "https://api.whatsapp.com/send?phone=+542983521842";
-const webURL = "https://tabernaculodemilag.wixsite.com/tabernculodemilagros";
 
 const OpenURLButton = ({ url, children }) => {
   const handlePress = useCallback(async () => {
@@ -32,128 +28,81 @@ const HomeScreen = () => {
   const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      <View style={styles.inLine}>
-        <FontAwesome name="circle" size={24} color="red" />
-        <Text style={styles.live}> EN VIVO </Text>
-      </View>
-    
-      <View style={styles.containerPlayer}>
-        <Image
-          style={styles.image}
-          /* source={status.isPlaying ? imagePlay : imagePause} */
-          source= {microphone}
-        />
-        <Video
-          ref={video}
-          style={styles.video}
-          source={{
-            uri: "https://node-09.zeno.fm/fnge482h268uv?rj-ttl=5&rj-tok=AAABfOY_WzkAQa2hCQKBbGYZyw",
-          }}
-          useNativeControls
-          /* resizeMode="contain" */
-          playsInline
-          isLooping
-          onPlaybackStatusUpdate={(status) => setStatus(() => status)} 
-        />
-
-        <View>
-          <FontAwesome 
-            name={status.isPlaying ? "pause" : "play"} 
-            size={42} 
-            color="#d6966d" 
-            onPress={() =>
-              status.isPlaying
-                ? video.current.pauseAsync()
-                : video.current.playAsync()
-            }
-          />
-         
-          <Foundation
-            name={!status.isMuted ? "volume" : "volume-strike"}
-            size={42}
-            style={styles.iconPause}
-            /* color={status.isPlaying ? "#d6966d" : "gray"} */
-            color="#d6966d"
-            onPress={() =>
-              status.isMuted
-                ? video.current.setIsMutedAsync(!status.isMuted)
-                : video.current.setIsMutedAsync(true)
-            }
-          />             
-         
-        </View>
-      </View>
-
+      <Text style={styles.title}> Tabernaculo de los milagros </Text>
+      <Image style={styles.image} source={status.isPlaying ? imagePlay : imagePause}/>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://node-09.zeno.fm/fnge482h268uv?rj-ttl=5&rj-tok=AAABfOY_WzkAQa2hCQKBbGYZyw',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
       <View style={styles.buttonContainer}>
-        <AntDesign name="instagram" size={36} color="#d6966d" onPress={ ()=>{ Linking.openURL(instagramURL) }}/>
-        <AntDesign name="youtube" size={36} color="#d6966d" onPress={ ()=>{ Linking.openURL(youtubeURL) }}/>
-        <FontAwesome name="whatsapp" size={36} color="#d6966d" onPress={ ()=>{ Linking.openURL(whatsappURL) }} />
-        <AntDesign name="link" size={36} color="#d6966d"  onPress={ ()=>{ Linking.openURL(webURL) }} />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Image
-          style={styles.icono}
-          source= {icono}
-        />
-      <Text style={styles.live}> Tabernáculo de milagros </Text>
+        <FontAwesome.Button 
+          style={styles.buttonPlay}
+          name= {status.isPlaying ? 'pause' : 'play'} 
+          backgroundColor="#3b5998" 
+          onPress={() =>  status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()}>
+        </FontAwesome.Button>
+  
+        <FontAwesome.Button 
+          disabled={status.isPlaying ? false : true}
+          name= {!status.isMuted ? 'volume-up' : 'volume-off'} 
+          backgroundColor="#3b5998" 
+          onPress={() => status.isPlaying ? video.current.setIsMutedAsync(!status.isMuted) : video.current.setIsMutedAsync(true)}>
+        </FontAwesome.Button>
       </View>
       
+      <View style={styles.buttonContainer}>
+        <OpenURLButton styles={styles.button} url={instagramURL}>Instagram</OpenURLButton>
+        <OpenURLButton styles={styles.button} url={youtubeURL}>YouTube</OpenURLButton>
+      </View>
+        
+       
+        
+
+
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#96613f",
     flex: 1,
-  },
-  containerPlayer: {
-    height: "70%",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: '#96613f'
   },
   title: {
     textAlign: "center",
-    color: "white",
+    color: 'white',
     fontSize: 30,
   },
   video: {
-    alignSelf: "center",
-    overflow: "hidden",
-    width: 0,
-    height: 0,
+    alignSelf: 'center',
+    overflow: 'hidden',
+    borderRadius: 15,
+    elevation: 5,
+    aspectRatio: 16 / 9,
   },
   image: {
-    width: "50%",
-    height: "50%",
-    marginBottom: 5, 
+    width: 300,
+    height: 150,
+    marginBottom: 5,
   },
   buttonContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  inLine: {
-    alignSelf: "flex-end",
-    flexDirection: "row",
     marginTop: 10,
-    top: 0,
-    right: 0,
+    width: '90%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
   },
-  live: {
-    fontFamily: "Waverly",
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-    alignSelf: "center",
-    justifyContent: "center"
-  },
-  icono: {
-    width: 100,
-    height: 75
-  },
-  iconPause: {
-    marginTop: 15,
+  buttonPlay: {
+    textAlign: 'center',
   }
 });
 
